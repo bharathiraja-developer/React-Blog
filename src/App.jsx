4a74d09e61fc2a68497192bb6ce7d4275e87972b
bharathiraja-developer <bharathiraja798@gmail.com>
2024-05-06 09:55:53 +0530

    Project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Components/Home";
 import FullStack from "./Components/FullStack";
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
@@ -7,13 +7,20 @@ import CyberSecurity from "./Components/CyberSecurity";
 import Career from "./Components/Career";
 
 function App() {
+  const [all, setAll] = useState(true);
   return (
     <div>
       <Router>
         <nav className="mt-4 border-bottom border-2 fw-medium">
           <ul className="nav nav-underline justify-content-center">
             <li className="nav-item">
-              <Link to="/" className="nav-link text-success">
+              <Link
+                onClick={() => setAll(true)}
+                to="/"
+                className={
+                  all ? "nav-link text-success active" : "nav-link text-success"
+                }
+              >
                 ALL
               </Link>
             </li>
@@ -21,22 +28,35 @@ function App() {
               <Link
                 to="/full-stack-development"
                 className="nav-link text-success"
+                onClick={() => setAll(false)}
               >
                 FULL STACK DEVELOPMENT
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/data-science" className="nav-link text-success">
+              <Link
+                to="/data-science"
+                className="nav-link text-success"
+                onClick={() => setAll(false)}
+              >
                 DATA SCIENCE
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/cyber-security" className="nav-link text-success">
+              <Link
+                to="/cyber-security"
+                className="nav-link text-success"
+                onClick={() => setAll(false)}
+              >
                 CYBER SECURITY
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/career" className="nav-link text-success">
+              <Link
+                to="/career"
+                className="nav-link text-success"
+                onClick={() => setAll(false)}
+              >
                 CAREER
               </Link>
             </li>
